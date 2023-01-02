d794cfbb1a19c15dd90e99a674c8f27d2e24acfe
ExceedPrince <tamas.bodorkos@hotmail.hu>
2023-01-02 02:15:53 +0100

    404 handle 2

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
 	return (
 		<div>
 			<h1>Oops! You seem to be lost.</h1>
-			<p>Here are some helpful links:</p>
+			<p>Here are some helpful links: </p>
 			<Link to='/'>Home</Link>
 		</div>
 	)
