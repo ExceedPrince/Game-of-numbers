59c6c95e39ff17c9f8de028fd8d260dbe5481927
ExceedPrince <tamas.bodorkos@hotmail.hu>
2023-01-02 20:08:53 +0100

    404 page added

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,10 +2,12 @@ import React, { Link } from "react-router-dom";
 
 export default function NotFound() {
 	return (
-		<div>
-			<h1>Oops! You seem to be lost.</h1>
-			<p>Here are some helpful links: </p>
-			<Link to='/'>Home</Link>
+		<div id="notFound">
+			<div id="notFound-inner">
+				<h1>Oops! You seem to be lost.</h1>
+				<p>If you want to go back to the main page, click on the link below:</p>
+				<Link to='/'>Main page</Link>
+			</div>
 		</div>
 	)
 }
